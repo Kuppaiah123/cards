9895a8b165465fea293a487e390dee0ef450704e
Kuppaiah123 <kuppaiah.devaraju94@gmail.com>
2023-01-20 11:29:34 +0530

    added config from git

--- src/main/java/com/app/cards/controller/CardsController.java
+++ src/main/java/com/app/cards/controller/CardsController.java
@@ -7,10 +7,16 @@ package com.app.cards.controller;
 
 import java.util.List;
 
+import com.app.cards.config.AccountServiceConfig;
 import com.app.cards.model.Cards;
 import com.app.cards.model.Customer;
+import com.app.cards.model.Properties;
 import com.app.cards.repository.CardsRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +33,9 @@ public class CardsController {
     @Autowired
     private CardsRepository cardsRepository;
 
+    @Autowired
+    private AccountServiceConfig accountServiceConfig;
+
     @PostMapping("/myCards")
     public List<Cards> getCardDetails(@RequestBody Customer customer) {
         List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
@@ -35,6 +44,14 @@ public class CardsController {
         } else {
             return null;
         }
+
+    }
+
+    @GetMapping("/cards/properties")
+    public String getPropertyDetails() throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        Properties properties = new Properties(accountServiceConfig.getMsg(), accountServiceConfig.getBuildVersion(), accountServiceConfig.getMailDetails(), accountServiceConfig.getActiveBranches());
+        return ow.writeValueAsString(properties);
 
     }
 
