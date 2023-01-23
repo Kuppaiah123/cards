ce1c2a35e9990ddc996ca70be2a67e18eea79bd5
Kuppaiah123 <kuppaiah.devaraju94@gmail.com>
2023-01-23 16:43:05 +0530

    added eureka

--- src/main/java/com/app/cards/CardsApplication.java
+++ src/main/java/com/app/cards/CardsApplication.java
@@ -3,11 +3,15 @@ package com.app.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.app.cards.controller") })
 @EnableJpaRepositories("com.app.cards.repository")
 @EntityScan("com.app.cards.model")
